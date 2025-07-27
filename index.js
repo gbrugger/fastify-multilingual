085fffbae100822d804df7018a79fdcb42f2c9d4
Guilherme D'Amato <guilherme.brugger@gmail.com>
2025-07-27 02:40:38 +0000

    refactor: remove NestedPhrases export from main index
    Remove NestedPhrases type export from main index.js as it's no longer needed in the public API. This simplifies the module exports and maintains a cleaner interface.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import fp from 'fastify-plugin';
-import fastifyMultilingual, { NestedPhrases } from './dist/plugin.js';
+import fastifyMultilingual from './dist/plugin.js';
 
 export default fp(fastifyMultilingual, {
   fastify: '5.x',
   name: 'fastify-multilingual',
 });
 
-export { fastifyMultilingual, NestedPhrases };
+export { fastifyMultilingual };
